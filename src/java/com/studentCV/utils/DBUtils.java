1717c583295630a8d267e3a25e67eca489b407d7
TungNT140601 <tungnt14062001@gmail.com>
2022-06-08 09:56:48 +0700

    08/06/2022

--- src/java/com/studentCV/utils/DBUtils.java
+++ src/java/com/studentCV/utils/DBUtils.java
@@ -4,10 +4,29 @@
  */
 package com.studentCV.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author ADMIN
  */
 public class DBUtils {
-    
+
+    public static Connection makeConnection() throws SQLException {
+        try {
+            //1.Load JDBC driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            //2.Create connection
+            String sqlConnectionString = "jdbc:sqlserver://localhost:1433;databaseName="
+                    + "OnlineCarRentingWebsite1";
+            Connection con = DriverManager.getConnection(sqlConnectionString, "sa", "a");
+            return con;
+        } catch (ClassNotFoundException | SQLException e) {
+
+        }
+        return null;
+    }
 }
