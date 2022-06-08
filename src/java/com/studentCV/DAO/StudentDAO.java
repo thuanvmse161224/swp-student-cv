83d31733e16f40dc51a4856bcf36fbbed057c964
TungNT140601 <tungnt14062001@gmail.com>
2022-06-08 10:00:35 +0700

    up DAO

---
+++ src/java/com/studentCV/DAO/StudentDAO.java
@@ -0,0 +1,193 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.studentCV.DAO;
+
+import com.studentCV.DTO.StudentDTO;
+import com.studentCV.utils.DBUtils;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author tungp
+ */
+public class StudentDAO {
+
+    private Connection con = null;
+    private PreparedStatement pstm = null;
+    private ResultSet rs = null;
+
+    private void closeConnection() throws SQLException {
+        if (rs != null) {
+            rs.close();
+        }
+        if (pstm != null) {
+            pstm.close();
+        }
+        if (con != null) {
+            con.close();
+        }
+    }
+
+    public ArrayList<StudentDTO> getAllStudent() throws NamingException, SQLException {
+
+        String sql = "SELECT [StudentId]\n"
+                + "      ,[JobPosition]\n"
+                + "      ,[EduQua]\n"
+                + "      ,[Experience]\n"
+                + "      ,[StuName]\n"
+                + "      ,[PhoneNum]\n"
+                + "      ,[Addr]\n"
+                + "      ,[Email]\n"
+                + "  FROM [StudentCV].[dbo].[Student]";
+
+        ArrayList<StudentDTO> lst = new ArrayList<>();
+
+        try {
+            con = DBUtils.makeConnection();
+
+            if (con != null) {
+                pstm = con.prepareStatement(sql);
+                rs = pstm.executeQuery();
+
+                while (rs.next()) {
+                    int id = rs.getInt("StudentId");
+                    String job = rs.getString("JobPosition");
+                    String edu = rs.getString("EduQua");
+                    String exp = rs.getString("Experience");
+                    String name = rs.getString("StuName");
+                    String phone = rs.getString("PhoneNum");
+                    String addr = rs.getString("Addr");
+                    String email = rs.getString("Email");
+
+                    StudentDTO stu = new StudentDTO(id, job, edu, exp, name, phone, addr, email);
+
+                    lst.add(stu);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+        return lst;
+    }
+
+    public boolean insert(StudentDTO stu) throws SQLException {
+        boolean check = false;
+        try {
+            String sql = "Insert Into [StudentCV].[dbo].[Student]"
+                    + "(      [JobPosition]\n"
+                    + "      ,[EduQua]\n"
+                    + "      ,[Experience]\n"
+                    + "      ,[StuName]\n"
+                    + "      ,[PhoneNum]\n"
+                    + "      ,[Addr]\n"
+                    + "      ,[Email]\n)"
+                    + "Values(?,?,?,?,?,?,?)";
+            DBUtils db = new DBUtils();
+            con = db.makeConnection();
+            pstm = con.prepareStatement(sql);
+            pstm.setString(1, stu.getJobPosition());
+            pstm.setString(2, stu.getEduQua());
+            pstm.setString(3, stu.getExperience());
+            pstm.setString(4, stu.getStuName());
+            pstm.setString(5, stu.getPhoneNum());
+            pstm.setString(6, stu.getAddr());
+            pstm.setString(7, stu.getEmail());
+
+            check = pstm.executeUpdate() > 0;
+        } finally {
+            closeConnection();
+        }
+
+        return check;
+    }
+
+    public boolean delete(String id) throws Exception {
+        boolean check = false;
+        try {
+            String sql = "Delete From Student\n"
+                    + "Where StudentId=?";
+            DBUtils db = new DBUtils();
+            con = db.makeConnection();
+            pstm = con.prepareStatement(sql);
+            pstm.setString(1, id);
+            check = pstm.executeUpdate() > 0;
+        } finally {
+            closeConnection();
+        }
+        return check;
+    }
+
+    public boolean update(StudentDTO stu) throws Exception {
+        String sql = "UPDATE [StudentCV].[dbo].[Student] SET"
+                + " JobPosition=?"
+                + ", EduQua=?"
+                + ", Experience=?"
+                + ", StuName=?"
+                + ", PhoneNum=?"
+                + ", Addr=?"
+                + ", Email=?"
+                + " WHERE StudentId=?";
+        try {
+            con = DBUtils.makeConnection();
+            if (con != null) {
+                pstm = con.prepareStatement(sql);
+
+                pstm.setString(1, stu.getJobPosition());
+                pstm.setString(2, stu.getEduQua());
+                pstm.setString(3, stu.getStuName());
+                pstm.setString(4, stu.getPhoneNum());
+                pstm.setString(5, stu.getAddr());
+                pstm.setString(6, stu.getEmail());
+                pstm.setInt(7, stu.getStudentId());
+                pstm.executeUpdate();
+                return true;
+            }
+        } finally {
+            closeConnection();
+        }
+        return false;
+    }
+
+    public StudentDTO getStudentById(int id) throws Exception {
+        StudentDTO result = null;
+        try {
+            String sql = "\"SELECT [StudentId]\\n\"\n"
+                    + "                + \"      ,[JobPosition]\\n\"\n"
+                    + "                + \"      ,[EduQua]\\n\"\n"
+                    + "                + \"      ,[Experience]\\n\"\n"
+                    + "                + \"      ,[StuName]\\n\"\n"
+                    + "                + \"      ,[PhoneNum]\\n\"\n"
+                    + "                + \"      ,[Addr]\\n\"\n"
+                    + "                + \"      ,[Email]\\n\"\n"
+                    + "                + \"  FROM [StudentCV].[dbo].[Student]\\n"
+                    + "Where StudentId=?\n";
+            DBUtils db = new DBUtils();
+            con = db.makeConnection();
+            pstm = con.prepareStatement(sql);
+            pstm.setInt(1, id);
+            rs = pstm.executeQuery();
+            if (rs.next()) {
+                String jobPosition = rs.getString("JobPosition");
+                String eduQua = rs.getString("EduQua");
+                String experience = rs.getString("Experience");
+                String stuName = rs.getString("StuName");
+                String phoneNum = rs.getString("PhoneNum");
+                String addr = rs.getString("Addr");
+                String email = rs.getString("Email");
+                result = new StudentDTO(id, jobPosition, eduQua, experience, stuName, phoneNum, addr, email);
+            }
+        } finally {
+            closeConnection();
+        }
+        return result;
+    }
+}
