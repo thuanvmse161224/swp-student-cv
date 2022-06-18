154ff86fc0d55b2163a8ac6fc425310dab6303f0
TungNT140601 <tungnt14062001@gmail.com>
2022-06-18 09:18:28 +0700

    update student

--- src/java/com/studentCV/DAO/CV_DAO.java
+++ src/java/com/studentCV/DAO/CV_DAO.java
@@ -141,7 +141,7 @@ public class CV_DAO {
                 + "      ,([StudentId]\n"
                 + "      ,[Status])\n"
                 + "Values(?,?)"
-                + " WHERE CvId=?";
+                + " WHERE CompanyId=?";
         try {
             con = DBUtils.makeConnection();
             if (con != null) {
