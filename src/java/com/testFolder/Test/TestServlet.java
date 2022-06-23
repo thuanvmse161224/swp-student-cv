c3ed970a3a612bda0d21e5dbc11d005d9a86a902
TungNT140601 <tungnt14062001@gmail.com>
2022-06-23 22:51:17 +0700

    add 3 file jsp

--- src/java/com/testFolder/Test/TestServlet.java
+++ src/java/com/testFolder/Test/TestServlet.java
@@ -30,7 +30,7 @@ public class TestServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.sendRedirect("view/about.jsp");
+        response.sendRedirect("view/index.jsp");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
