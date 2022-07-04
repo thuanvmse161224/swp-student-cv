24ef71a7425c88bc7bf8ecab83bee3f0a478a7ca
TungNT140601 <tungnt14062001@gmail.com>
2022-07-04 20:52:34 +0700

    add file

--- src/java/com/studentCV/utils/Constant.java
+++ src/java/com/studentCV/utils/Constant.java
@@ -13,7 +13,7 @@ public class Constant {
     public static String GOOGLE_CLIENT_ID = "367863869149-p52rvo311i3dh3oolbjcimig62rh97af.apps.googleusercontent.com";
 
     public static String GOOGLE_CLIENT_SECRET = "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";
-    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/LoginGoogleController";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/StudentCV/LoginGoogleController";
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
     public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
     public static String GOOGLE_GRANT_TYPE = "authorization_code";
--- src/java/com/studentCV/utils/DBUtils.java
+++ src/java/com/studentCV/utils/DBUtils.java
@@ -21,8 +21,8 @@ public class DBUtils {
 
             //2.Create connection
             String sqlConnectionString = "jdbc:sqlserver://localhost:1433;databaseName="
-                    + "OnlineCarRentingWebsite1";
-            Connection con = DriverManager.getConnection(sqlConnectionString, "sa", "a");
+                    + "StudentCV";
+            Connection con = DriverManager.getConnection(sqlConnectionString, "sa", "1234567890");
             return con;
         } catch (ClassNotFoundException | SQLException e) {
 
